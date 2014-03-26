50ea5d4eb6a6dcf6894b1e12afd12eef6a30f135
Gracjan Polak <gracjan@scrive.com>
2014-03-26 13:24:39 +0100

    Basic parsing is working

--- src/Data/Unjson.hs
+++ src/Data/Unjson.hs
@@ -1,28 +1,68 @@
 module Data.Unjson
-  ( Unjson(..)
-  , field
-  , fieldOpt
+  ( fieldBy
+  , Documentation(..)
+  , document
+  , parse
+  , Result(..)
   )
 where
 
 import qualified Data.Aeson as Aeson
 import qualified Data.Text as Text
 import Data.Typeable
+import Data.Monoid
 import Control.Applicative
 import Control.Applicative.Free
+import Data.Scientific
+import Data.Attoparsec.Number
+import qualified Data.HashMap.Strict as HashMap
 
-class Unjson a where
-  unjson :: UnjsonX a
 
 data UnjsonX' a
-  = Field Text.Text
-  | FieldOpt Text.Text
+  = Field Text.Text Text.Text (Aeson.Value -> Result a)
+  deriving (Typeable, Functor)
+
+{-
+instance Functor UnjsonX' where
+  fmap f (Field a b k) = Field a b ((fmap . fmap) f k)
+-}
+
+type UnjsonX a = Ap UnjsonX' a
+
+data Result a = Result a Problems
+  deriving (Functor, Show, Ord, Eq)
+
+type Problems = [Problem]
+
+type Problem = Text.Text
+
+data Documentation
+  = Documentation Text.Text                    -- ^ description of this particular item
+                  [(Text.Text,Documentation)]  -- ^ description of its parts, key-value
   deriving (Eq, Ord, Show, Typeable)
 
-type UnjsonX = Ap UnjsonX'
 
-field :: Text.Text -> UnjsonX a
-field name = liftAp (Field name)
+fieldBy :: (Aeson.Value -> Result a) -> Text.Text -> Text.Text -> UnjsonX a
+fieldBy f name docstring = liftAp (Field name docstring f)
 
-fieldOpt :: Text.Text -> UnjsonX (Maybe a)
-fieldOpt name = liftAp (FieldOpt name)
+documentF :: UnjsonX' a -> Documentation
+documentF (Field key docstring p) = Documentation "" [(key,Documentation docstring [])]
+--documentF (FieldOpt key docstring) = Documentation "" [(key,Documentation docstring [])]
+
+document :: UnjsonX a -> Documentation
+document (Pure x) = Documentation "" []
+document (Ap a b) = Documentation (a1 <> b1) (a2 <> b2)
+  where
+    Documentation a1 a2 = documentF a
+    Documentation b1 b2 = document b
+
+parse :: UnjsonX a -> Aeson.Value -> Result a
+parse (Pure v) _ = Result v []
+parse (Ap (Field key _ ap) b) v = Result (bv av) (aproblems <> bproblems)
+  where
+    Result av aproblems = case v of
+                            Aeson.Object o -> case HashMap.lookup key o of
+                                                Just v2 -> ap v2
+                                                Nothing -> Result (error "key does not exists in object") ["key does not exists in object"]
+                            _ -> Result (error "trying to lookup a key in non-object") ["trying to lookup a key in non-object"]
+    Result bv bproblems = parse b v
--- test/Test.hs
+++ test/Test.hs
@@ -6,22 +6,45 @@ import qualified Data.Text as Text
 import Data.Typeable
 import Data.Unjson
 import Control.Applicative
+import qualified Data.Aeson as Aeson
+import Data.Scientific
+import Data.Attoparsec.Number
+import Data.Monoid
 
 -- As an example we will use a hypothetical configuration data.
 -- There are some mandatory fields and some optional fields.
 data Konfig =
      Konfig { konfigHostname :: Text.Text
             , konfigPort     :: Int
-            , konfigUsername :: Maybe Text.Text
+            , konfigUsername :: Text.Text
             }
   deriving (Eq,Ord,Show,Typeable)
 
-instance Unjson Konfig where
-  unjson = pure Konfig
-           <*> field "hostname"
-           <*> field "port"
-           <*> fieldOpt "username"
+
+gText (Aeson.String s) = Result s []
+gText _ = Result msg [msg]
+  where
+    msg = "trying to get a string from something that is not a string, sorry"
+
+gInt (Aeson.Number v) | base10Exponent v >= 0 = Result (fromIntegral $ coefficient v) []
+gInt (Aeson.Number v) = Result (error msg) [Text.pack msg]
+  where msg = "number is not integer: " <> show v <> ", base10Exponent=" <> show (base10Exponent v)
+gInt _ = Result (error msg) [msg]
+  where
+    msg = "trying to get a string from something that is not a number, sorry"
+
+unjsonKonfig = pure Konfig
+           <*> fieldBy gText "hostname" "docstring for hostname"
+           <*> fieldBy gInt "port" "docstring for port"
+           <*> fieldBy gText "username" "docstring for username"
+
+parsedKonfig = parse unjsonKonfig undefined
+
+json1 :: Aeson.Value
+Just json1 = Aeson.decode "{\"hostname\": \"www.example.com\", \"port\": 12345, \"username\": \"user1\"}"
 
 main = do
+    print (document unjsonKonfig)
+    print (parse unjsonKonfig json1)
     putStrLn "This test always fails!"
     exitFailure
