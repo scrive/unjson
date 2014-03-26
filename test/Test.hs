ff105bd2c4cd6917befe6b675bdbf9d95ef64938
Gracjan Polak <gracjan@scrive.com>
2014-03-26 08:01:37 +0100

    Add example structure to deserialize

--- test/Test.hs
+++ test/Test.hs
@@ -2,6 +2,18 @@ module Main where
 
 import System.Exit (exitFailure)
 
+import qualified Data.Text as Text
+import Data.Typeable
+
+-- As an example we will use a hypothetical configuration data.
+-- There are some mandatory fields and some optional fields.
+data Konfig =
+     Konfig { konfigHostname :: Text.Text
+            , konfigPort     :: Int
+            , konfigUsername :: Maybe Text.Text
+            }
+  deriving (Eq,Ord,Show,Typeable)
+
 main = do
     putStrLn "This test always fails!"
     exitFailure
