02107be2bd04a3f19515a2a53524ba61b8dc28a5
Gracjan Polak <gracjan@scrive.com>
2014-06-26 20:13:31 +0200

    Add simple example in tests

--- test/Test.hs
+++ test/Test.hs
@@ -454,3 +454,22 @@ filterOutAnsiTillEndOfMulticharSequence (c : rest) | c >= '@' = filterOutAnsi re
 filterOutAnsiTillEndOfMulticharSequence (c : rest) =
   filterOutAnsiTillEndOfMulticharSequence rest
 filterOutAnsiTillEndOfMulticharSequence [] = []
+
+
+data Example = Example
+   { exampleName :: Text.Text,
+     exampleArray :: [Int],
+     exampleOptional :: Maybe Bool }
+
+unjsonExample :: UnjsonDef Example
+unjsonExample = objectOf $ pure Example
+  <*> field' "name"
+          exampleName
+          "Name used for example"
+  <*> fieldDefBy "array_of_ints" []
+          exampleArray
+          "Array of integers, optional, defaults to empty list"
+          arrayOf'
+  <*> fieldOpt' "optional_bool"
+          exampleOptional
+          "Optional boolean"
